a0f94f8baa2b1b5d5c3e2e4d40a3af775b3c551f
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-12-09 21:58:59 +0530

    App Setup

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,48 +1,10 @@
 import "./App.css";
 import "./style.css";
-import ChildComponentWithInputBox from "./ChildComponentWithInputBox";
-import { useRef, forwardRef, useState } from "react";
 function App() {
   return (
     <>
-      <div className="App">
-        <h1>HOC</h1>
-        <HocRed />
-        <HocGreen />
-      </div>
+      <h1>Hello World!</h1>
     </>
-  );
-}
-
-function HocRed() {
-  return (
-    <div style={{ background: "red" }}>
-      <Counter />
-    </div>
-  );
-}
-
-function HocGreen() {
-  return (
-    <div style={{ background: "green" }}>
-      <Counter />
-    </div>
-  );
-}
-
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <h3>{count}</h3>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Update
-      </button>
-    </div>
   );
 }
 
