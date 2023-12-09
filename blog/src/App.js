bebf6993256f9e03f45e53254921e178d40d6da9
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-12-09 16:20:22 +0530

    Use Memo

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,41 @@
 import "./App.css";
-import User from "./User";
 import "./style.css";
 import { Button } from "react-bootstrap";
+import { useState, useMemo } from "react";
 function App() {
-  function alertAtParent(data) {
-    alert("Welcome : " + data);
-  }
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(10);
+
+  const mutiCountMemo = useMemo(
+    function multicount() {
+      console.warn("multicount");
+      return count * 5;
+    },
+    [count]
+  );
+
   return (
     <>
-      This is Parent Component
-      <User property={alertAtParent} />
+      <div className="App">
+        <h1>Use Memo Hook in React</h1>
+        <h2>Count :{count}</h2>
+        <h2>item : {item}</h2>
+        <h2>{mutiCountMemo}</h2>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          Update Count
+        </button>
+        <button
+          onClick={() => {
+            setItem(item * 10);
+          }}
+        >
+          Update Item
+        </button>
+      </div>
     </>
   );
 }
