9a94ed08c7105cf33b4ce84497e38210ee4c9e09
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-12-10 15:18:01 +0530

    Use Navigate Hook

--- blog/src/component/Home.js
+++ blog/src/component/Home.js
@@ -1,12 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import About from "./About";
 
 function Home() {
+  const navigate = useNavigate();
+  function conditionalRendering(url) {
+    if (url == "about") navigate("/about");
+    else navigate("/filter");
+  }
   return (
     <>
       <h1>Home Page</h1>
       <p>This Home Page of our awesome Application </p>
       <p>And we are learning the React Aaplication</p>
       <Link to="/about"> Click here to Navigate to About Page</Link>
+      <br />
+      <button onClick={() => conditionalRendering("filter")}>
+        Navigate to Filter Page
+      </button>
+      <br />
+      <button onClick={() => conditionalRendering("about")}>
+        Navigate to About Page
+      </button>
     </>
   );
 }
