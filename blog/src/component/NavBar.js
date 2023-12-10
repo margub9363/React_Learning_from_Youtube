201254eac466d9603c590a95987e83b7119ad9fa
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-12-10 11:46:05 +0530

    Using NavLink

--- blog/src/component/NavBar.js
+++ blog/src/component/NavBar.js
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <div>
       <ul>
         <li>
-          <Link to="/">Home Page </Link>
+          <NavLink className="nav-bar-NavLink " style={{ color: "red" }} to="/">
+            Home Page{" "}
+          </NavLink>
         </li>
         <li>
-          <Link to="/about">About Page </Link>
+          <NavLink className="nav-bar-NavLink " to="/about">
+            About Page{" "}
+          </NavLink>
         </li>
       </ul>
     </div>
