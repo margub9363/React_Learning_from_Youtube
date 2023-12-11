bdd966c7cd0bc81236f7e683dc9764db67eac9f3
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-12-11 10:00:11 +0530

    Getting Previous State in useState hook

--- blog/src/App.js
+++ blog/src/App.js
@@ -12,29 +12,21 @@ import Other from "./component/Other";
 import Protected from "./component/Protected";
 import User from "./component/User";
 import "./style.css";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
+  const [count, setCount] = useState(1);
+  function udpateCounter() {
+    for (let i = 0; i < 5; i++) {
+      setCount((previoiusState) => previoiusState + 1);
+    }
+  }
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Protected Component={Home} />} />
-          <Route path="/about" element={<About />} />
-          <Route
-            path="/user/:name/:profession"
-            element={<Protected Component={User} />}
-          />
-          <Route path="/filter" element={<Filter />} />
-          <Route path="/contact/" element={<Protected Component={Contact} />}>
-            <Route path="company" element={<Company />} />
-            <Route path="channel" element={<Channel />} />
-            <Route path="other" element={<Other />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-          <Route path="/*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="App">
+        <h1>{count}</h1>
+        <button onClick={udpateCounter}>Click Me to Update Counter</button>
+      </div>
     </>
   );
 }
